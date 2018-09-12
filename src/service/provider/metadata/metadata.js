9653af5b216fea70be1656e76b340c893b30827b
Rie Miyazaki <rie.miyazaki@axa.co.jp>
2018-09-12 12:52:28 +0900

    WIP Added url encode

--- src/service/provider/metadata/metadata.js
+++ src/service/provider/metadata/metadata.js
@@ -1,10 +1,10 @@
 import request from 'request-promise';
-
+import urlencode from 'urlencode';
 
 function analyse(textString) {
     return new Promise((resolve, reject)=>{
         const API_KEY = process.env['METADATA_API_KEY'];
-        request(`http://ap.mextractr.net/ma9/emotion_analyzer?out=json&apikey=${API_KEY}&text=${textString}`)
+        request(`http://ap.mextractr.net/ma9/emotion_analyzer?out=json&apikey=${API_KEY}&text=${urlencode(textString)}`)
             .then(function (result) {
                 resolve(result);
             })
